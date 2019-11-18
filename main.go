f43d4b8c5930a5b5408188f275d35a26a1a264b1
DJSIer <djsier.dtm@gmail.com>
2019-11-18 16:00:03 +0900

    GCASL2 add

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ import (
 
 const version = "0.1.21"
 
+// ShareCode URL id binding
+type ShareCode struct {
+	ID string `uri:"id" binding:"required"`
+}
+
 func main() {
 	port := os.Getenv("PORT")
 
@@ -22,16 +27,30 @@ func main() {
 	}
 	router := gin.Default()
 	router.LoadHTMLGlob("WOCASL2/*.html")
-	
-	router.Static("./assets","WOCASL2/assets")
-	router.Static("./src","WOCASL2/src")
 
+	router.Static("./assets", "WOCASL2/assets")
+	router.Static("./src", "WOCASL2/src")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"title": "GCASL Online",
 		})
 	})
+
+	router.GET("/GCASL2/:id", func(c *gin.Context) {
+		var share ShareCode
+		if err := c.ShouldBindUri(&share); err != nil {
+			c.JSON(400, gin.H{"msg": err})
+			return
+		}
+		c.JSON(200, gin.H{"uuid": share.ID})
+	})
+	//debug : curl -F "code=value1" localhost:8080/GCASL2/add
+	router.POST("/GCASL2/add", func(c *gin.Context) {
+		postCode := c.PostForm("code")
+		c.JSON(200, gin.H{"code": postCode})
+	})
+
 	router.POST("/GCASL", func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		postCode := c.PostForm("code")
